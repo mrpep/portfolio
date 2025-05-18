67223b48c2304b6e3b3b32a30f24ea87b22d136a
mrpep <leonardodpepino@gmail.com>
2025-05-18 05:04:18 +0000

    Deploying to gh-pages from @ mrpep/portfolio@d97da8ca18568353b8e97010121b6cae1ffb23f5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "books-the-godfather",
+        },{id: "post-diving-deep-into-k-means",
+        
+          title: "Diving deep into K-Means",
+        
+        description: "Let&#39;s implement K-Means from scratch in PyTorch",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2023/kmeans/";
+          
+        },
+      },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
